b935df512411f89adb003a43e7685132619d31c9
Joshua Akangah <akangah89@hmail.com>
2020-07-11 17:19:58 +0000

    Voting for each poll works

--- glopoll/src/App.js
+++ glopoll/src/App.js
@@ -5,6 +5,7 @@ import Poll from './UIComponents/Poll.js';
 import { Container, Row, Col, CardGroup } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import User from './Logic/UserProfile';
+import {v4 as uuid} from 'uuid';
 
 // global variable
 var newUser;
@@ -14,58 +15,51 @@ class App extends React.Component {
 		super();
 
 		newUser = new User('1');
-		localStorage.setItem(newUser.id.toString(), newUser.getPoll())
 
 		this.state = {
-			polls: [
-				{
-					id: 1,
-					title: "Flask or Django?",
-					text: "Which do you prefer?",
-					yesVotes: 5,
-					noVotes: 3,
-					voted: 8,
-					yesPercent: 62.5,
-					noPercent: 37.5,
-					choiceOne: 'Flask',
-					choiceTwo: 'Django',
-					willExpireOn: '1 48',
-					isExpired: true,
-				},
-			],
+			polls: JSON.parse(localStorage.getItem('polls'))
 		}
 
 		localStorage.setItem('polls', JSON.stringify(this.state.polls))
 	}
 
 	voteYes = (e, id) => {
-		var btnArray = document.getElementsByClassName(`${id}poll`);
-		for (let i=0; i<btnArray.length; i++){
-			btnArray[i].disabled = true;
-		}
-		this.setState(this.state.polls.map((poll) => {
-			if (poll.id === id){
-				poll.yesVotes += 1
-				poll.voted += 1
-				newUser.addPoll(poll.id);
-				this.findPercent(id);
+		if (!newUser.getPoll().includes(id)){
+			var btnArray = document.getElementsByClassName(`${id}poll`);
+			for (let i=0; i<btnArray.length; i++){
+				btnArray[i].disabled = true;
 			}
-		}))
+			this.setState(this.state.polls.map((poll) => {
+				if (poll.id === id){
+					poll.yesVotes += 1
+					poll.voted += 1
+					newUser.addPoll(poll.id);
+					this.findPercent(id);
+				}
+			}))
+			newUser.addPoll(id);
+			localStorage.setItem('polls', JSON.stringify(this.state.polls))
+		}
+		
 	}
 
 	voteNo = (e, id) => {
-		var btnArray = document.getElementsByClassName(`${id}poll`);
-		for (let i=0; i<btnArray.length; i++){
-			btnArray[i].disabled = true;
-		}
-		this.setState(this.state.polls.map((poll) => {
-			if (poll.id === id){
-				poll.noVotes += 1
-				poll.voted += 1
-				newUser.addPoll(poll.id);
-				this.findPercent(id);
+		if (!newUser.getPoll().includes(id)){
+			var btnArray = document.getElementsByClassName(`${id}poll`);
+			for (let i=0; i<btnArray.length; i++){
+				btnArray[i].disabled = true;
 			}
-		}))
+			this.setState(this.state.polls.map((poll) => {
+				if (poll.id === id){
+					poll.noVotes += 1
+					poll.voted += 1
+					newUser.addPoll(poll.id);
+					this.findPercent(id);
+				}
+			}))
+			newUser.addPoll(id);
+			localStorage.setItem('polls', JSON.stringify(this.state.polls))
+		}
 	}
 	
 
@@ -88,10 +82,10 @@ class App extends React.Component {
 
 
 	addPoll = (e) => {
-		e.preventDefault();
+		console.log(JSON.parse(localStorage.getItem('polls')))
 		this.setState({
 			polls: [
-				...this.state.polls, 
+				...JSON.parse(localStorage.getItem('polls')), 
 				{
 					id: 10,
 					title: e.target.title.value,
@@ -107,21 +101,35 @@ class App extends React.Component {
 					isExpired: false,
 				}
 			]
-		})
+		});
 
-		localStorage.setItem('polls', JSON.stringify(this.state.polls))
+
+
+		localStorage.setItem('polls', JSON.stringify([
+			...this.state.polls, {
+					id: 10,
+					title: e.target.title.value,
+					text: e.target.description.value,
+					yesVotes: 0,
+					noVotes: 0,
+					voted: 0,
+					yesPercent: 0,
+					noPercent: 0,
+					choiceOne: e.target.choiceOne.value,
+					choiceTwo: e.target.choiceTwo.value,
+					willExpireOn: '7 30',
+					isExpired: false,
+				}]))
 	}
 
 	render() {
 		let polls = JSON.parse(localStorage.getItem('polls'));
-		console.log(polls)
 		return (
 			<div>
 		    	<NavBar polls={polls} voteYes={this.voteYes} voteNo={this.voteNo} killPoll={this.killPoll} addPoll={this.addPoll} />
 			</div>
 		);
 	}
-	
 }
 
 export default App;
--- glopoll/src/Logic/UserProfile.js
+++ glopoll/src/Logic/UserProfile.js
@@ -3,20 +3,27 @@
 export default class User{
 	constructor(id){
 		this.id = id;
-		this.votedPolls = [];
+		this.votedPolls = ["NONE"];
+		if (localStorage.getItem(this.id) === null){
+			localStorage.setItem(this.id, this.votedPolls)
+		}
 	}
 
 	addPoll = (poll) => {
 		// check if poll does not already exist
 		if (!this.votedPolls.includes(poll)){
 			this.votedPolls.push(poll);
-			localStorage.setItem(this.id.toString(), this.getPoll());
+			localStorage.setItem(this.id, JSON.stringify(this.votedPolls));
 		}
 		
 	}
 
+	getPollList = () => {
+		return this.votedPolls
+	}
+
 	getPoll = () => {
-		return this.votedPolls;
+		return JSON.parse(localStorage.getItem(this.id));
 	}
 }
 
